633ad95e2144e7d6d6863848591b4d8c8f7fda44
Davis Clark <141876928+davisclrk@users.noreply.github.com>
2024-09-28 19:06:37 -0400

    Update App.tsx

--- client/frontend/src/App.tsx
+++ client/frontend/src/App.tsx
@@ -7,6 +7,7 @@ const App: React.FC = () => {
   const [linkToken, setLinkToken] = useState<string | null>(null);
   const [isLoading, setIsLoading] = useState<boolean>(true);
   const [isAuthenticated, setIsAuthenticated] = useState(false);
+  const [transactions, setTransactions] = useState<any[]>([]);
 
   useEffect(() => {
     const fetchLinkToken = async () => {
@@ -42,16 +43,57 @@ const App: React.FC = () => {
     setIsAuthenticated(true);
     // only show plaid link after create account. we can assume users who login have already linked their plaid accounts in the past
   };
-
-  return (
-    <div>
-      {isAuthenticated ? (
-        <PlaidLinkButton linkToken={linkToken!} />
-      ) : (
-        <AuthScreen onLoginSuccess={handleLoginSuccess} />
-      )}
-    </div>
-  );
-}
-
+  
+  const getTransactions = async () => {
+      try {
+        const userId = localStorage.getItem('userId');
+        const requestBody = {
+          userId: userId
+        };
+        console.log("getting transactions");
+        const response = await fetch("http://localhost:4000/api/getTransactions", { method: "POST", headers: {
+          'Content-Type': 'application/json'
+        }, body: JSON.stringify(requestBody) });
+        console.log("response: ", response);
+        if (response.status === 200) {
+          const data = await response.json();
+          console.log("transactions: ", data);
+          setTransactions(data);
+        } else {
+          console.log("Error fetching transactions");
+        }
+      } catch (error) {
+        console.error('Error fetching transactions:', error);
+      }
+    }
+  
+    return (
+      <div>
+        {isAuthenticated ? (
+          <>
+            <PlaidLinkButton linkToken={linkToken!} />
+            <button onClick={() => getTransactions()}>Get transaction history</button>
+            <div>
+            {transactions.length > 0 ? (
+              <ul>
+                {transactions.map((transaction, index) => (
+                  <li key={index}>
+                    <p>{transaction.name}</p>
+                    <p>{transaction.amount}</p>
+                    <p>{transaction.date}</p>
+                  </li>
+                ))}
+              </ul>
+            ) : (
+              <p>No transactions found</p>
+            )}
+          </div>
+          </>
+        ) : (
+          <AuthScreen onLoginSuccess={handleLoginSuccess} />
+        )}
+      </div>
+    );
+  }
+  
 export default App;
