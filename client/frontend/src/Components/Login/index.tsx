58632a3d04332d8d1671819125d1e73eba2c461b
Davis Clark <141876928+davisclrk@users.noreply.github.com>
2024-09-28 23:56:00 -0400

    Update index.tsx

--- client/frontend/src/Components/Login/index.tsx
+++ client/frontend/src/Components/Login/index.tsx
@@ -2,7 +2,6 @@ import React, { useState } from 'react';
 import './login.css';
 import {Doughnut} from 'react-chartjs-2';
 import { Chart, ArcElement, Tooltip, Legend } from 'chart.js';
-import { loadGetInitialProps } from 'next/dist/shared/lib/utils';
 
 
 interface AuthScreenProps {
